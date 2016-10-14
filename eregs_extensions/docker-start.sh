04cac21a8870e814bf7d38dbe2d5d3647b20a532
James C. Scott <jamesc.scott@gsa.gov>
2016-10-14 15:59:39 -0400

    Upload all XMLs, not just the latest

--- eregs_extensions/docker-start.sh
+++ eregs_extensions/docker-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "LOCAL_XML_PATHS = ['/app/input/']" > local_settings.py
 ./manage.py migrate
-eregs pipeline 27 646 http://web:8000/api --only-latest
+eregs pipeline 27 646 http://web:8000/api
